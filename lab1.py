496cd48327ab2ff8e623cb9be75bf5304696d8bc
ShootThePoop <147568900+ShootThePoop@users.noreply.github.com>
2025-03-19 10:14:02 +1100

    dice roller worked on and done, lab 1 and 2 of project 5 done

--- lab1.py
+++ lab1.py
@@ -8,18 +8,22 @@
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 # Create a list named favorite_foods and add at least five different food items to it
-
+favourite_foods = ["banana", "apple", "pickles", "twitzle"]
 
 # Change the third item in the list to a different food
-
+favourite_foods[2] = "pringles"
 
 # Print out only the first and fourth items in the list
+print(favourite_foods[0])
+print(favourite_foods[3])
 
 
 # Ask the user for a food item
-
+food = input("Please enter a food item to add to your list: ")
 
 # Append the user's food item to the list
-
+favourite_foods.append(food)
 
 # Print out the entire list of food items
+for i in favourite_foods:
+    print(i)
--- lab2.py
+++ lab2.py
@@ -12,18 +12,25 @@ fruits = ["Grape", "Apple", "Lemon", "Cherry", "Date", "Elderberry", "Fig",  "Ho
 
 
 # Remove the third fruit from the list using the pop() method.
-
+fruits.pop(2)
 
 # Use the remove() method to remove a fruit of your choice from the list.
-
-
+fruits.remove("Fig")
+last_fruit = fruits.pop()
+print(fruits)
+print("Removed fruit:", last_fruit)
 # Search for "Apple" in the list and print a message saying whether or not it was found.
-
+if "Apple" in fruits:
+    print("You have apples in your list.")
 
 # Sort the List in alphabetical order.
-
+fruits.sort()
+print(fruits)
 
 # Create a new list that contains a slice of the first 5 items
-
+fivefruits = fruits[0:5]
+print(fivefruits)
 
 # Print out the new sliced list using a for loop
+for i in fivefruits:
+    print(i)
