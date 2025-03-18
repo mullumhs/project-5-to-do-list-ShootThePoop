f00f60fa0ad166033cbdd6297fc4043eee8311ba
ShootThePoop <147568900+ShootThePoop@users.noreply.github.com>
2025-03-19 10:40:17 +1100

    to do list project 5 started and lab 3 project 5 done

--- to-do-list.py
+++ to-do-list.py
@@ -6,3 +6,22 @@
 - Class: Software Engineering
 - Description: Complete a functional dice roller app in Python.
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+import time
+
+print("Welcome to ---===THE+LIST===---")
+time.sleep(1)
+def Menu():
+    print("Would you like to:")
+    print("1. Add to the list")
+    print("2. Edit the list")
+    print("3. Remove something from the list")
+    choice = input("Make your choice: ")
+    while True:
+        try:
+            int(choice)
+
+        except:
+            print("Please enter a number.")
+
+    
+Menu()
